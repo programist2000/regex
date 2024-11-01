d830ce062e2e8cc5589aa8d7ff8802bb53145c1f
Alexander <ichiro_san@mail.ru>
2024-11-01 20:48:43 +0500

    test for mozilla

--- script/script.js
+++ script/script.js
@@ -31,8 +31,8 @@ document.getElementById("inp").addEventListener("keyup", function(){
                 for (let i = 0; i < result.length; i++) {
                     result[i] = result[i].slice(1);
                 }
-                document.getElementById('result-row').innerHTML = result.join(' ')
-                document.getElementById('result-col').innerHTML = result.join('\n')     
+                document.getElementById('result-row').value = result.join(' ')
+                document.getElementById('result-col').value = result.join('\n')     
             }
                 
     } else {
@@ -46,8 +46,8 @@ document.getElementById("inp2").addEventListener("keyup", function(){
 
     if (dTwo != 0) {
         resultTwo = dTwo.match(/\d+/gmi);
-        document.getElementById('result-row').innerHTML = resultTwo.join(' ')
-        document.getElementById('result-col').innerHTML = resultTwo.join('\n')      
+        document.getElementById('result-row').value = resultTwo.join(' ')
+        document.getElementById('result-col').value = resultTwo.join('\n')      
     } else {
         clearinfo()
     }    
